1f6bc6895b7bb9e54107f842fd5c9b26c66e1568
wolwemaan <ettiennev@mail.usf.edu>
2020-01-19 18:29:02 -0500

    Update program with comment

---
+++ HelloConsoleApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Hello Console App
+namespace HelloConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            //System.Threading.Thread.Sleep(5000);
+        }
+    }
+}
